cb4ea0eb591e03c76dc5b7f2bffe8a68503b48f8
leminh <quanlycanhan1312@gmail.com>
2022-02-28 21:56:32 +0700

    done

--- Api/src/main/java/API/repository/LoanRepository.java
+++
@@ -1,7 +0,0 @@
-package API.repository;
-
-import API.entity.Loan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoanRepository extends JpaRepository<Loan,Integer> {
-}
